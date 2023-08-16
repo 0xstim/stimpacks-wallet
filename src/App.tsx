e417eb63d96400afe92fdc588d36adc105c3b917
0xStim <0xStim@computer.local>
2023-08-16 17:59:50 +0700

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from "@/components/ui/button"
+import { Button } from "./components/ui/button"
 import {
   Download,
   Upload,
@@ -14,7 +11,7 @@ import {
   TabsContent, 
   TabsList, 
   TabsTrigger 
-} from "@/components/ui/tabs"
+} from "./components/ui/tabs"
 
 
 function App() {
